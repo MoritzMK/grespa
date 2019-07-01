67ff29be786c54738b37105732ecc998b7151a4c
Danny <nguyen@petesso.de>
2019-07-01 14:47:46 +0200

    current parser

--- flask-bs/venue_scraper.py
+++ flask-bs/venue_scraper.py
@@ -7,13 +7,11 @@ SEARCH_PATTERN = 'http://portal.core.edu.au/conf-ranks/?search=&by=all&source=CO
 # 1 bis 33 also range(1,34)
 
 
-def downloadPage(start=0):
-    url = SEARCH_PATTERN+'1'
+def downloadPage(number):
+    url = SEARCH_PATTERN + f'{number}'
     with urllib.request.urlopen(url) as response:
-        with open("asdf.html", "w+") as file:
-            html = response.read().decode('utf-8')
-            # file.write(html)
-            return html
+        html = response.read().decode('utf-8')
+        return html
 
 
 def generateVenueItem():
@@ -41,66 +39,50 @@ def generateVenueList(row, venue_list):
 
 
 if __name__ == "__main__":
-
-    while i in range(1,34):
+    venue_list = []
+    for i in range(1, 34):
         # html runterladen
+        html = downloadPage(i)
+
+        dom = etree.HTML(html)
+        even_row = dom.xpath('//tr[@class="evenrow"]/td/text()')
+        odd_row = dom.xpath('//tr[@class="evenrow"]/td/text()')
+        j = 0
+        for element in even_row:
+            even_row[j] = element.lstrip('\n ').rstrip()
+            j += 1
+
+        j = 0
+        for element in odd_row:
+            odd_row[j] = element.lstrip('\n ').rstrip()
+            j += 1
+
         # liste erweitern
+        generateVenueList(even_row, venue_list)
+        generateVenueList(odd_row, venue_list)
         pass
     # liste in json umwandeln
-
-    html = downloadPage()
-
-    # print(html)
-
-    dom = etree.HTML(html)
-    title_even = dom.xpath('//tr[@class="evenrow"]/td/text()')
-    title_odd = dom.xpath('//tr[@class="oddrow"]/td/text()')
-    rest_even = dom.xpath(
-        '//tr[@class="evenrow"]/td[@class="nowrap"]/text()')
-    rest_odd = dom.xpath(
-        '//tr[@class="oddrow"]/td[@class="nowrap"]/text()')
-
-    even_row = dom.xpath('//tr[@class="evenrow"]/td/text()')
-    odd_row = dom.xpath('//tr[@class="evenrow"]/td/text()')
-
-    # test = dom.xpath(
-    #     '//tr[@class="evenrow"]/td/text() | //tr[@class="evenrow"]/td[@class="nowrap"]/text()')
-    # print(test)
-
-    # remove whitespaces
-
-    i = 0
-    for element in even_row:
-        even_row[i] = element.lstrip('\n ').rstrip()
-        i += 1
-
-    i = 0
-    for element in odd_row:
-        odd_row[i] = element.lstrip('\n ').rstrip()
-        i += 1
-
-    # i = 0
-    # for element in title_even:
-    #     title_even[i] = element.lstrip('\n ').rstrip()
-    #     i += 1
-
-    # i = 0
-    # for element in title_odd:
-    #     title_odd[i] = element.lstrip('\n ').rstrip()
-    #     i += 1
-
-    # i = 0
-    # for element in rest_even:
-    #     rest_even[i] = element.lstrip('\n ').rstrip()
-    #     i += 1
-
-    # print("DESCRIPTION: ", even_row)
-    venue_list = []
-    generateVenueList(even_row, venue_list)
-    generateVenueList(odd_row, venue_list)
-    # print(venue_list)
     y = json.dumps(venue_list, indent=4)
-    with open("venues.json","w+") as file:
+    with open("venues.json", "w+") as file:
         file.write(y)
+
+    # title_even = dom.xpath('//tr[@class="evenrow"]/td/text()')
+    # title_odd = dom.xpath('//tr[@class="oddrow"]/td/text()')
+    # rest_even = dom.xpath(
+    #     '//tr[@class="evenrow"]/td[@class="nowrap"]/text()')
+    # rest_odd = dom.xpath(
+    #     '//tr[@class="oddrow"]/td[@class="nowrap"]/text()')
+    # even_row = dom.xpath('//tr[@class="evenrow"]/td/text()')
+    # odd_row = dom.xpath('//tr[@class="evenrow"]/td/text()')
+
+    # j = 0
+    # for element in even_row:
+    #     even_row[j] = element.lstrip('\n ').rstrip()
+    #     j += 1
+
+    # j = 0
+    # for element in odd_row:
+    #     odd_row[j] = element.lstrip('\n ').rstrip()
+    #     j += 1
 
     pass
