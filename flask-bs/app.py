db8f40da2320b7a19fe9a7db9e2406c967258743
Moritz M. Kolb <moritz.kolb@mailbox.org>
2019-07-02 23:07:03 +0200

    added some style things. added some metrics. currently working on hindex.

--- flask-bs/app.py
+++ flask-bs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from items.encoder import AuthorEncoder
 # from flask_bootstrap import Bootstrap
 
@@ -16,17 +16,10 @@ def index():
 
 @app.route('/author/<string:author_id>', methods=['GET'])
 def get_author(author_id):
-    # 1. Check if crawler is running
-    # If not:
-    # 2. Check if data is present
-    # If not:
-    # 3. Start crawling
-
-    # Check if crawler is still running
-    app.logger.debug('Check if scraping is in progress.')
-    data = scraper.scrapePage(author_id)
+    year = request.args.get('year')
+    data = scraper.scrapePage(author_id, year)
     return jsonify(data=AuthorEncoder().encode(data))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
