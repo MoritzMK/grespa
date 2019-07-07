3d2e4660c36a152df47850c48c708b935cdde4c8
Danny <nguyen@petesso.de>
2019-07-07 20:08:28 +0200

    matcher update, fuzzy still not final

--- flask-bs/json_match.py
+++ flask-bs/json_match.py
@@ -5,108 +5,129 @@ from fuzzywuzzy import fuzz
 
 class RankingMatcher():
 
+    def __init__(self, *args, **kwargs):
+        self.rank_dict = {"A*": 0, "A": 0, "B": 0, "C": 0, "other": 0}
+        self.fuzzy = 0
+
+    def clearDict(self):
+        self.rank_dict = {"A*": 0, "A": 0, "B": 0, "C": 0, "other": 0}
+
+    def addRank(self, rank):
+        if rank in self.rank_dict:
+            self.rank_dict[rank] += 1
+        else:
+            self.rank_dict['other'] = 1
+
     def hasAcronym(self, title):
         x = re.search("\(([^)]+)\)", title)
         if x != None:
             acronym = title[int(x.span()[0])+1:int(x.span()[1])-1]
+            print("acronym:", acronym)
             return(acronym)
         else:
             return None
 
-    def fuzzymatch(self, a, b):
-        print(fuzz.ratio(a, b))
+    def searchAcronym(self, acronym):
+        with open("venues.json", "r") as venues:
+            venue_list = json.load(venues)
+            for venue in venue_list:
+                if venue['acronym'] == acronym:
+                    self.addRank(venue['rank'])
+                    return venue['rank']
 
-    def searchVenues(self,):
+    def searchVenuesFuzzy(self, title):
+        with open("venues.json", "r") as venues:
+            venue_list = json.load(venues)
+            print("TITLE:", title)
+            fuzzy_list = []
+            for venue in venue_list:
+                if fuzz.ratio(venue['title'], title) > 70:
+                    fuzzy_list.append(
+                        {'title': venue['title'], 'ratio': fuzz.ratio(venue['title'], title), 'rank': venue['rank']})
+
+                highest_match = None
+                for fuzzy_match in fuzzy_list:
+                    if highest_match == None:
+                        fuzzy_match = highest_match
+                    else:
+                        if fuzzy_match['ratio'] > highest_match['ratio']:
+                            highest_match = fuzzy_match
+
+                if not highest_match == None:
+                    print(highest_match)
+                    if highest_match['rank'] in self.rank_dict:
+                        self.rank_dict[highest_match['rank']] += 1
+                    else:
+                        self.rank_dict["other"] += 1
+
+    def searchVenues(self, title):
         with open("venues.json", "r") as venues:
             venue_list = json.load(venues)
             for venue in venue_list:
                 if title == venue['title']:
-                    if venue['rank'] in rank_dict:
-                        rank_dict[venue['rank']] += 1
-                        match = True
-                    else:
-                        rank_dict["other"] += 1
-        pass
+                    addRank(venue['rank'])
+                    return venue['rank']
 
-    def searchJournals(self,):
+    def searchJournals(self, title):
         with open("journals.json", "r") as journals:
             journal_list = json.load(journals)
             for journal in journal_list:
                 if title == journal['title']:
-                    if journal['rank'] in rank_dict:
-                        rank_dict[journal['rank']] += 1
-                        match = True
-                    else:
-                        rank_dict["other"] += 1
-        pass
+                    addRank(journal['rank'])
+                    return journal['rank']
 
+# returns rank dict given string title
     def matchOne(self, title):
-        rank_dict = {"A*": 0, "A": 0, "B": 0, "C": 0, "other": 0}
-
-        match = False
-        if "journal" in title:
-            searchJournals()
+        match = None
+        acronym = self.hasAcronym(title)
+        if acronym != None:
+            match = self.searchAcronym(acronym)
+            if not match == None:
+                print("match acro")
         else:
-            searchVenues()
-        print(rank_dict)
-        return rank_dict
+            if "journal" in title:
+                match = self.searchJournals(title)
+                if not match == None:
+                    print("match journal")
+            else:
+                match = self.searchVenues(title)
+                if not match == None:
+                    print("match venue")
+        # if no match => fuzzy match
+        if match == None:
+            print("no match => fuzzy")
+            match = self.searchVenuesFuzzy(title)
+            if not match == None:
+                print("match fuzzy")
+            else:
+                print("no match for real")
 
-    def matchAll(self, ):
-        rank_dict = {"A*": 0, "A": 0, "B": 0, "C": 0, "other": 0}
-        with open("venues.json", "r") as venues:
-            venue_list = json.load(venues)
-            with open("journals.json", "r") as journals:
-                journal_list = json.load(journals)
-                with open("papers.json", "r") as papers:
-                    paper_list = json.load(papers)
-                    for paper in paper_list:
-                        acronym = self.hasAcronym(paper['venue'])
-                        if acronym != None:
-                            # search for acronym
-                            for venue in venue_list:
-                                if venue['acronym'] == acronym:
-                                    print('acronym match')
-                                    if venue['rank'] in rank_dict:
-                                        rank_dict[venue['rank']] += 1
-                                    else:
-                                        rank_dict[venue['rank']] = 1
-                        else:
-                            # string match title
-                            print(
-                                "No acronym for ", paper["title"], ". Searching by given venue: ", paper['venue'])
-                            match = False
-                            # substring = paper['venue']
-                            # substring = substring[:-1]
-                            # print("Substring: ", substring)
-                            for venue in venue_list:
-                                if paper['venue'] == venue['title']:
-                                    if venue['rank'] in rank_dict:
-                                        rank_dict[venue['rank']] += 1
-                                        match = True
-                                    else:
-                                        rank_dict[venue['rank']] = 1
-                                        match = True
-                            if match == False:
-                                # search in journals
-                                print(
-                                    "No match found for ", paper["title"], "in conferences. Searching in journals")
-                                for journal in journal_list:
-                                    if paper['venue'] in journal['title']:
-                                        if journal['rank'] in rank_dict:
-                                            rank_dict[journal['rank']] += 1
-                                            match = True
-                                        else:
-                                            rank_dict[journal['rank']] = 1
-                                            match = True
-                                if match == False:
-                                    print("No match found")
-        print(rank_dict)
+        if not match == None:
+            return match
+
+# loads titles via json
+    def matchAllJson(self):
+        with open("papers.json", "r") as papers:
+            paper_list = json.load(papers)
+            i = 1
+            for paper in paper_list:
+                print(i, paper["title"])
+                self.matchOne(paper["venue"])
+                i += 1
+        print(self.rank_dict)
+
+# loads titles via string array
+    def matchAllString(self, title_list):
+        i = 1
+        for paper in title_list:
+            print(i, paper)
+            self.matchOne(paper)
+            i += 1
+        print(self.rank_dict)
 
 
 if __name__ == "__main__":
     matcher = RankingMatcher()
-    matcher.matchAll()
-    # matcher.fuzzymatch(
-    #     "Proc. Int. ACM SIGIR Conference on Research and Development in Information",
-    #     "ACM International Conference on Research and Development in Information Retrieval")
+    print(matcher.matchOne("Proc. ACM Symposium on Document Engineering(DocEng)"))
+    # matcher.matchAllJson()
     pass
