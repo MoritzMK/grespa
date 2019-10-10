e6db34302fc3fef21eb2f92181d16f74ad85c8c1
moritzmk <moritz.kolb@mailbox.org>
2019-10-10 11:52:45 +0200

    Removed folder old

--- old/gscholar_scraper/prepare-db.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-# Prepares the database for scrapy and the web application
-
-import gscholar_scraper.database as db
-
-if __name__ == '__main__':
-    print('Preparing database for use.')
-    db.create_relations()
